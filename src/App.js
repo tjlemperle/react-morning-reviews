e0b9af0311151de0ea7dfebc265c5dc12642ede1
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-01-30 09:01:34 -0700

    Got ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import List from './Components/List'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="movie-list-title">Let's do this thing!</h1>
+      <h1 className="movie-list-title">The most amazing movie list</h1>
+      <List />
     </div>
   )
 }
