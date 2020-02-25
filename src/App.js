0cf4114566e6267106c138a8626bd9680daf5c7d
Lucas <lucas@Ashleys-MacBook-Pro.local>
2020-02-25 14:27:23 -0700

    Initialized app for building together in class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,1 @@
-import React from 'react'
-import List from './Components/List'
-import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="movie-list-title">The most amazing movie list</h1>
-      <List />
-    </div>
-  )
-}
-
-export default App
